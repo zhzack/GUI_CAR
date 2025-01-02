2ca6daf06dfd5e82566e50d867920d12fb69028b
zhengzhi <1018784543@qq.com>
2025-01-02 17:04:34 +0800

    111

--- car_canvas.py
+++ car_canvas.py
@@ -36,7 +36,7 @@ class CarCanvas(QGraphicsView):
         self.setResizeAnchor(QGraphicsView.AnchorUnderMouse)
         self.queue = None
         self.fence_mode_active = False
-        self.mouse_move_active = False
+        self.mouse_move_active = True
         self.highlighted = False
 
         self.fence_tool = parent.fence_tool
@@ -97,8 +97,8 @@ class CarCanvas(QGraphicsView):
 
     def init_servo_ws2812(self):
         # 创建串口管理实例
-        # self.manager = SerialManager(port_by_keyword='20',baudrate=115200)
-        self.manager = TCPServer(host='192.168.47.13', port=80)
+        self.manager = SerialManager(port_by_keyword='7',baudrate=115200)
+        # self.manager = TCPServer(host='192.168.47.13', port=80)
 
         self.ws2812 = Ws2812(self.manager)
         self.ws2812.num_strips = 15
@@ -329,7 +329,7 @@ class CarCanvas(QGraphicsView):
 
                 x = value['x']
                 y = value['y']
-                if key == 'UWB1':
+                if key != 'UWB1':
                     print(x, y)
                     angle = self.calculate_angle(-x, y)
                     self.set_angle(angle)
